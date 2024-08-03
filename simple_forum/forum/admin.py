526911f20f0f9c75a32ed8ef2876f20a2c396af7
aibekov_nurkerim <aibekovnurkerim@gmail.com>
2024-08-03 17:54:01 +0600

    Добавил роль модератора

--- simple_forum/forum/admin.py
+++ simple_forum/forum/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, Permission
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+class UserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if change and obj.username == 'moder':
+            group, created = Group.objects.get_or_create(name='Moderators')
+            if created:
+                permissions = Permission.objects.filter(
+                    content_type__model__in=['topic', 'reply']
+                )
+                group.permissions.set(permissions)
+            obj.groups.add(group)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
